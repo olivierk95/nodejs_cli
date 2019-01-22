5ac868a726d04d7a64f64e136ffb7871f79d18a1
olivierk95 <keutgensolivier@gmail.com>
2019-01-22 11:07:37 +0100

    intégration des packages chalk et ora

--- index.js
+++ index.js
@@ -2,60 +2,91 @@
 
 const validator = require("email-validator");
 const axios = require("axios");
+const chalk = require('chalk');
+const ora = require('ora');
 
 //check if email is valid
-
 var account = process.argv[2];
 var validEmail = validator.validate(account);
 
-if(validEmail) {
+//spinner for loading
+const spinner = ora('');
+spinner.start();
+setTimeout(() => {
+    spinner.color = 'yellow';
+    spinner.text = 'Loading breaches';
+});
+
+if (validEmail) {
+
     axios({
-        method:'get',
-        url:`https://haveibeenpwned.com/api/v2/breachedaccount/${account}`,
-        headers: {"User-Agent": "terminal-mailbreachchecker"},
-    })
-    .then(function (response) {
-        var breachArray = response.data.map(data=>data.Name);
-        console.log(breachArray.toString());
-    })
-    .catch (function (error) {
-        const status = error.response&&error.response.status;
-        
-        switch(status) {
-            
-            case 400:
-                console.log("Speak nicely and check your input twice ; bad request");   
-                break;
-            
-            case 403:
-                console.log("You forget to match the user agent, what were you thinking about you perv?");  
-                break;
-            
-            case 404:
-                console.log("No breach, you are good brother!");
-            
-            case 429:
-                console.log("Come later my friend, the website is under attack ; too many requests.");
-                break;
 
-            default:
-                console.log("Unknow error, wtf have done buddy? Check your connection");
+            method: 'get',
+            url: `https://haveibeenpwned.com/api/v2/breachedaccount/${account}`,
+            headers: {
+                "User-Agent": "terminal-mailbreachchecker"
+            },
 
-        }
-    });
-} else if(!validEmail) {
-    console.log("Invalid mail address, try again mate!")
+        })
+
+        .then(function (response) {
+
+            spinner.text = 'ATTENTION, breaches found';
+            spinner.warn();
+
+            var breachArray = response.data.map(data => data.Name);
+            console.log(chalk.white.bgRed(breachArray.toString()));
+
+        })
+
+        .catch(function (error) {
+
+            const status = error.response && error.response.status;
+
+            switch (status) {
+
+                case 400:
+                    spinner.text = 'error';
+                    spinner.fail();
+                    
+                    console.log(chalk.red("Speak nicely and check your input twice ; bad request"));
+                    break;
+
+                case 403:
+                    spinner.text = 'error';
+                    spinner.fail();
+                    
+                    console.log(chalk.red("You forget to match the user agent, what were you thinking about you perv?"));
+                    break;
+
+                case 404:
+                    spinner.text = 'SUCCESS';
+                    spinner.succeed();
+                    
+                    console.log(chalk.white.bgGreen("No breach, you are good brother!"));
+                    break;
+
+                case 429:
+                    spinner.text = 'error';
+                    spinner.fail();
+                    
+                    console.log(chalk.red("Come later my friend, the website is under attack ; too many requests."));
+                    break;
+
+                default:
+                    spinner.text = 'error';
+                    spinner.fail();
+                    
+                    console.log(chalk.red("Unknow error, wtf have done buddy? Check your connection"));
+
+            }
+
+        });
+
+} else if (!validEmail) {
+    spinner.text = 'mistake';
+    spinner.fail();
+    
+    console.log(chalk.white.bgRedBright("Invalid mail address, try again mate!"));
+
 };
-
-
-
-// //By version in URL:
-// GET "https://haveibeenpwned.com/api/v2/breachedaccount/{account}"
-
-// //By api-version header:
-// GET "https://haveibeenpwned.com/api/breachedaccount/{account}"
-// api-version: 2
-
-// //By content negotiation:
-// GET "https://haveibeenpwned.com/api/breachedaccount/{account}"
-// Accept: application/vnd.haveibeenpwned.v2+json
